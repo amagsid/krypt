e3897b2f9ccd27aa6d5834a204c3a0af2119534a
Ahmad <ahmad.magdy@ambassadors.com>
2022-10-18 15:19:24 +0200

    finished basic layout and form

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -7,12 +7,27 @@ import { Loader } from './';
 const commonStyles =
 	'min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white';
 
+const Input = ({ placeholder, name, type, value }) => (
+	<input
+		placeholder={placeholder}
+		name={name}
+		type={type}
+		step="0.0001"
+		value={value}
+		onChange={(e) => handleChange(e, name)}
+		className=" my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism "
+	/>
+);
+
+const handleSubmit = () => {};
+
 function Welcome() {
 	const connectWallet = () => {};
 	return (
+		// welcome/connect wallet
 		<div className="flex w-full justify-center items-center">
-			<div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
-				<div className="flex flex-1 justify-start flex-col md:mr-10">
+			<div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+				<div className="flex flex-1 justify-start flex-col mf:mr-10">
 					<h1 className="text-3xl sm:text-5xl text-white text-gradient py-1 ">
 						Send Crypto <br /> across the world
 					</h1>
@@ -33,6 +48,67 @@ function Welcome() {
 						<div className={`rounded-bl-2xl ${commonStyles}`}> Web 3.0</div>
 						<div className={` ${commonStyles}`}> Low Fees</div>
 						<div className={`rounded-br-2xl ${commonStyles}`}>BlockChain</div>
+					</div>
+				</div>
+
+				{/* Eth card */}
+				<div className="text-white">
+					<div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
+						<div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism">
+							<div className="flex justify-between flex-col w-full h-full ">
+								<div className="flex justify-between items-start">
+									<div className=" w-10 h-10 rounded-full border-2 border-white flex justify-center items-center ">
+										<SiEthereum fontSize={21} color="#fff" />
+									</div>
+									<BsInfoCircle fontSize={17} color="#fff" />
+								</div>
+								<div>
+									<p className="text-white font-light text-xs">
+										0xshhdjkuwhak.....nah7wj
+									</p>
+									<p className="text-white font-semibold text-lg">Ethereum</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					{/* form */}
+					<div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
+						<Input
+							placeholder="Address To"
+							name="addressTo"
+							type="text"
+							handleChange={() => {}}
+						/>
+						<Input
+							placeholder="Amout (ETH)"
+							name="amount"
+							type="number"
+							handleChange={() => {}}
+						/>
+						<Input
+							placeholder="Keyword (Gif)"
+							name="keywoerd"
+							type="text"
+							handleChange={() => {}}
+						/>
+						<Input
+							placeholder="Enter Message"
+							name="message"
+							type="text"
+							handleChange={() => {}}
+						/>
+						<div className="h-[1px] w-full bg-gray-400 my-2" />
+						{true ? (
+							<Loader />
+						) : (
+							<button
+								type="button"
+								onClick={handleSubmit}
+								className="text-whit mt-2 border-[1px] w-full p-2 border-[#3d4f7c] rounded-full cursor-pointer"
+							>
+								Send Now
+							</button>
+						)}
 					</div>
 				</div>
 			</div>
