98d709e6bb61a3d2866cfc7db7570d53628aeaa9
Ahmad <ahmad.magdy@ambassadors.com>
2022-10-18 08:44:45 +0200

    worked on navbar and welcome page

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -40,7 +40,7 @@ const NavBar = () => {
 					/>
 				)}
 				{toggleMenu && (
-					<ul className="z-10 fixed top-0 -right-2 p-3 w-[70vw] ">
+					<ul className="z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2x1 md:hidden list-none flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in ">
 						{' '}
 						<li className="text-xl w-full my-2">
 							{' '}
